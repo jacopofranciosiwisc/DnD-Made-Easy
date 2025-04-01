11329dbd7bfe0c7389b979ec7b229770f42e77d7
Jacopo Franciosi <jfranciosi@wisc.edu>
2025-03-31 21:30:44 -0500

    Implemented registering functionality

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,16 +3,25 @@ const router = express.Router();
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res) => {
 	const { username, email, password } = req.body;
 	try {
+		const saltRounds = 10;
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
+
 		const newUser = await prisma.user.create({
 			data: {
 				username,
 				email,
-				password,
+				password: hashedPassword,
 			},
+		});
+
+		console.log('User successfully created in the database:', {
+			username: newUser.username,
+			email: newUser.email,
 		});
 
 		res.status(201).send({
