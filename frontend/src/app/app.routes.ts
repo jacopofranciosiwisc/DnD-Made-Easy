5c7de63f4fc384e0d0593405fd1be30034a0fd36
Jacopo Franciosi <jfranciosi@wisc.edu>
2025-04-06 18:12:36 -0500

    added routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent, title: 'DnD Made Easy' },
+  { path: 'register', component: RegisterComponent },
+];
