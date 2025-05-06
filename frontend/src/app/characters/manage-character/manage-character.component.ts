913349a57e3d9f03a88c8b2055d1a4c90a2d8ef9
Jacopo Franciosi <jfranciosi@wisc.edu>
2025-05-05 21:56:24 -0500

    Added character creation introduction and profile integration

--- frontend/src/app/characters/manage-character/manage-character.component.ts
+++ frontend/src/app/characters/manage-character/manage-character.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ProfileComponent } from './profile/profile.component';
 import { AbilityScoresComponent } from './ability-scores/ability-scores.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ActionsComponent } from './actions/actions.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import { NotebookComponent } from './notebook/notebook.component';
-import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Profile {
+  name: string;
+  race: string;
+  class: string;
+  age: string;
+  level: number;
+  hp: number;
+  ac: number;
+  initiative: number;
+  profilePicture: string;
+}
 
 @Component({
   selector: 'dnd-manage-character',
@@ -17,21 +31,57 @@ import { HttpClient } from '@angular/common/http';
     ActionsComponent,
     InventoryComponent,
     NotebookComponent,
+    FormsModule,
+    CommonModule,
   ],
   templateUrl: './manage-character.component.html',
   styleUrl: './manage-character.component.scss',
 })
 export class ManageCharacterComponent implements OnInit {
-  constructor(private http: HttpClient) {}
   defaultCharacter: any = {};
   modifiedCharacter: any = {};
+  // Show the profile modal if profile is not yet created or when editing
+  showProfileModal: boolean = true;
+
+  // Hold the profile data locally
+  profile: Profile = {
+    name: '',
+    race: '',
+    class: '',
+    age: '',
+    level: 1,
+    hp: 1,
+    ac: 1,
+    initiative: 1,
+    profilePicture: '',
+  };
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get('assets/character.json').subscribe((data) => {
       this.defaultCharacter = data;
+      // Override profile from file if it exists:
+      if (this.defaultCharacter.profile) {
+        this.profile = this.defaultCharacter.profile;
+        // Hide modal if profile is already set
+        if (this.profile.name) {
+          this.showProfileModal = false;
+        }
+      }
       this.modifiedCharacter = JSON.parse(
         JSON.stringify(this.defaultCharacter)
       );
     });
+  }
+
+  saveProfile() {
+    this.defaultCharacter.profile = { ...this.profile };
+    this.showProfileModal = false;
+  }
+
+  editProfile() {
+    // Show modal again for editing profile
+    this.showProfileModal = true;
   }
 }
--- frontend/src/app/characters/manage-character/profile/profile.component.ts
+++ frontend/src/app/characters/manage-character/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,7 +8,13 @@ import { Router } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  @Input() profile: any = {};
+
+  ngOnInit(): void {
+    console.log(this.profile);
+  }
+
   constructor(private router: Router) {}
 
   goBack() {
